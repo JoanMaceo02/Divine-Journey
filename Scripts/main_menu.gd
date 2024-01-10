2b6c689479871b4759ec100e8923b739e591c040
Joan Maceo <126512019+JoanMaceo02@users.noreply.github.com>
2024-01-10 14:54:21 +0100

    Commit del play Button
    Este commit contiene la funcionalidad del botón de Play, de momento carga la escena que será la escena principal

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,27 @@
+class_name MainMenu
+extends Node2D
+
+
+
+# onready is the same if we put the var on the _ready function
+# We read the buttons play and quit as variables
+@onready var play_button = $MarginContainer/HBoxContainer/VBoxContainer/PlayButton as Button
+@onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton as Button
+
+# We do preload in order to stored teh scene in memory at the very beginning,
+# This is helpful in order to avoid having delay when loading large scene
+@onready var start_level = preload("res://Scenes/mainScene.tscn") as PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play_button.button_down.connect(on_start_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func on_start_pressed () -> void:
+	get_tree().change_scene_to_packed(start_level)
