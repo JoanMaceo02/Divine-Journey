0613e287fee7ecd8063aeed214ed0afc145af3f9
Joan Maceo <126512019+JoanMaceo02@users.noreply.github.com>
2024-02-20 11:33:06 +0100

    Commit del tiempo de la partida
    Se puede ver el tiempo transcurrido desde que se inicia la partida, faltaría configurar el tiempo cuando pasa una hora.

---
+++ Scripts/player_HUD.gd
@@ -0,0 +1,27 @@
+extends Control
+
+var seconds = 0
+var secondsFirstDigit = 0
+var secondsSecondDigit = 0
+var minutesFirstDigit = 0
+var minutesSecondsDigit = 0
+
+@onready var secondsLabelFirstDigit = $MarginContainer/VBoxContainer/HBoxContainer/Seconds1
+@onready var secondsLabelSecondDigit = $MarginContainer/VBoxContainer/HBoxContainer/Seconds2
+@onready var minutesLabelFirstDigit = $MarginContainer/VBoxContainer/HBoxContainer/Minutes1
+@onready var minutesLabelSecondsDigit = $MarginContainer/VBoxContainer/HBoxContainer/Minutes2
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	seconds += delta
+	secondsFirstDigit = str(int(seconds) % 10)
+	secondsSecondDigit = str((int(seconds) / 10) % 6)
+	minutesFirstDigit = str((int(seconds) / 60) % 10)
+	minutesSecondsDigit = str(int(seconds) / 600 % 6)
+	
+	secondsLabelFirstDigit.text = secondsFirstDigit
+	secondsLabelSecondDigit.text = secondsSecondDigit
+	minutesLabelFirstDigit.text = minutesFirstDigit
+	minutesLabelSecondsDigit.text = minutesSecondsDigit
